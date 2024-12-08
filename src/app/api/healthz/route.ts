6082f83d570bbecbca7f2c87c4cd60822bf7f051
Fallah Andy Prakasa <me@flla.my.id>
2024-12-08 13:23:16 +0700

    feat: create health check endpoint

---
+++ src/app/api/healthz/route.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import os from "os-utils";
+import { sql } from "drizzle-orm";
+
+import { db } from "@bolabali/server/db";
+
+export const GET = async () => {
+  // Check if the database is connected, and response time
+  const start = Date.now();
+  await db.execute(sql`SELECT 1`);
+  const end = Date.now();
+
+  // Monitor system resources
+  const uptime = os.sysUptime();
+  const processUptime = os.processUptime();
+  const freeMemory = os.freemem();
+  const totalMemory = os.totalmem();
+  const freeMemoryPercentage = os.freememPercentage();
+  const loadAverage = os.allLoadavg();
+  const cpuUsagePercentage = await new Promise<number>((resolve) => {
+    os.cpuUsage((v) => resolve(v));
+  });
+
+  return Response.json({
+    status: "ok",
+    db: {
+      connected: true,
+      responseTime: `${end - start}ms`,
+    },
+    system: {
+      uptime,
+      processUptime,
+      freeMemory,
+      totalMemory,
+      freeMemoryPercentage,
+      loadAverage,
+      cpuUsagePercentage,
+    },
+  });
+};
