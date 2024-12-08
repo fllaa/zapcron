4d95f2ad0450d91c345307d4b08cc1a3f0f363c8
Fallah Andy Prakasa <me@flla.my.id>
2024-12-08 10:10:38 +0700

    Revert "feat(server): setup job scheduler"
    This reverts commit 1dd410e8021ede3817571d74301a1b4aa914ac22.

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 
-import { Scheduler } from "./server/scheduler";
-
 const port = parseInt(process.env.PORT ?? "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, turbo: true, customServer: true });
@@ -16,15 +14,6 @@ void app.prepare().then(() => {
       void handle(req, res, parsedUrl);
     }
   }).listen(port);
-
-  const scheduler = new Scheduler();
-
-  scheduler.start();
-
-  process.on("SIGINT", () => {
-    scheduler.stop();
-    process.exit(0);
-  });
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
