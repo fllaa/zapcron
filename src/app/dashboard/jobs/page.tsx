d7a25deb26f22d31940acf53fbff385f4b0ec570
Fallah Andy Prakasa <me@flla.my.id>
2024-12-19 13:15:55 +0700

    chore: remove guard prefetch job api

--- src/app/dashboard/jobs/page.tsx
+++ src/app/dashboard/jobs/page.tsx
@@ -1,5 +1,4 @@
 import { api, HydrateClient } from "@bolabali/trpc/server";
-import { auth } from "@bolabali/server/auth";
 import { JobsWrapper } from "@bolabali/components/features/jobs";
 import { HeaderPage } from "@bolabali/components/common";
 
@@ -10,14 +9,14 @@ export default async function JobsPage({
 }) {
   const { limit, page } = await searchParams;
 
-  const session = await auth();
-
-  if (session?.user) {
-    void api.job.getAll.prefetch({
+  await api.job.getAll
+    .prefetch({
       limit: !isNaN(parseInt(limit!)) ? parseInt(limit!) : undefined,
       page: !isNaN(parseInt(page!)) ? parseInt(page!) : undefined,
+    })
+    .catch(() => {
+      // ignore
     });
-  }
 
   return (
     <div className="p-2 md:mx-4 md:my-2 md:p-4 lg:mx-4 lg:my-4 lg:p-6 xl:mx-16 xl:my-10 xl:p-8">
