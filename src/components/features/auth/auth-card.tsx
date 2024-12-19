93cb718242dd6ea67d6b2b09b41e8f647df7f5aa
Fallah Andy Prakasa <me@flla.my.id>
2024-12-19 14:40:55 +0700

    chore(auth): remove login form

--- src/components/features/auth/auth-card.tsx
+++ src/components/features/auth/auth-card.tsx
@@ -1,73 +1,23 @@
 "use client";
 
-import React, { useState } from "react";
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Input,
-  Divider,
-} from "@nextui-org/react";
+import React from "react";
+import { Button, Card, CardHeader, CardBody } from "@nextui-org/react";
 import { signIn } from "next-auth/react";
-import { Eye, EyeClosed } from "lucide-react";
 import { FaGithub, FaMicrosoft } from "react-icons/fa6";
 
 const AuthCard = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePassword = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
     <Card className="w-full max-w-[400px]">
       <CardHeader>
         <div className="w-full text-center">
           <h2 className="text-lg font-semibold">Sign In to Your Account</h2>
           <p className="text-sm text-gray-500">
-            Enter your email and password to sign in
+            Sign in to access your account
           </p>
         </div>
       </CardHeader>
       <CardBody>
-        <div className="space-y-4">
-          <Input
-            autoFocus
-            isClearable
-            label="Email"
-            placeholder="Enter your email"
-            variant="bordered"
-          />
-          <Input
-            type={showPassword ? "text" : "password"}
-            label="Password"
-            placeholder="Enter your password"
-            variant="bordered"
-            endContent={
-              <button
-                aria-label="toggle password visibility"
-                className="text-gray-500 focus:outline-none"
-                type="button"
-                onClick={togglePassword}
-              >
-                {showPassword ? <EyeClosed size={20} /> : <Eye size={20} />}
-              </button>
-            }
-          />
-        </div>
-      </CardBody>
-      <CardFooter>
         <div className="w-full space-y-4 text-center">
-          <Button type="submit" color="primary">
-            Sign In
-          </Button>
-          <div className="flex items-center justify-center gap-2">
-            <Divider />
-            <span className="text-sm text-gray-500">OR</span>
-            <Divider />
-          </div>
           <Button
             color="default"
             variant="bordered"
@@ -85,7 +35,7 @@ const AuthCard = () => {
             Sign In with Microsoft
           </Button>
         </div>
-      </CardFooter>
+      </CardBody>
     </Card>
   );
 };
